45241ef2171e89197997369d0d43f6e9721417a6
varathan <varathansuyambu95@gmail.com>
2018-07-24 20:31:23 +0530

    Employee Controller has been added

---
+++ WebAPIDemo/WebAPIDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (dbsampleEntities1 EntityObject = new dbsampleEntities1())
+            {
+                return EntityObject.Employees.ToList();
+            }
+        }
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                using (dbsampleEntities1 entityObj = new dbsampleEntities1())
+                {
+                    Employee EmpObj = entityObj.Employees.FirstOrDefault(emp => emp.EmpID == id);
+                    if (EmpObj == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with the id=" + id + " not found");
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK,EmpObj);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Post([FromBody]Employee EmpObj)
+        {
+            try
+            {
+                using (dbsampleEntities1 entityObj = new dbsampleEntities1())
+                {
+                    entityObj.Employees.Add(EmpObj);
+                    entityObj.SaveChanges();
+                    var message= Request.CreateResponse(HttpStatusCode.Created, EmpObj);
+                    message.Headers.Location = new Uri(Request.RequestUri + EmpObj.EmpID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (dbsampleEntities1 entityObj = new dbsampleEntities1())
+                {
+                    Employee EmpObj = entityObj.Employees.FirstOrDefault(emp => emp.EmpID == id);
+                    if (EmpObj == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with the id=" + id + " not found");
+                    }
+                    else
+                    {
+                        entityObj.Employees.Remove(EmpObj);
+                        entityObj.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK,"Deleted");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Put(int id,[FromBody]Employee EmpObj)
+        {
+            try
+            {
+                using (dbsampleEntities1 entityObj = new dbsampleEntities1())
+                {
+                    Employee Emp = entityObj.Employees.FirstOrDefault(emp => emp.EmpID == id);
+                    if (EmpObj == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, "Employee with the id=" + id + " not found");
+                    }
+                    else
+                    {
+                        Emp.EmpName = EmpObj.EmpName;
+                        Emp.EmpSalary = EmpObj.EmpSalary;
+                        Emp.EmpLocation = EmpObj.EmpLocation;
+                        entityObj.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK, "Updated");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
